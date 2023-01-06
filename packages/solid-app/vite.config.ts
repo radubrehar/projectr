8f2e7a8868ed8f0a4dfa0a607ea55f4e65bc31a7
radubrehar <radu@jslog.com>
2023-01-06 14:59:08 +0200

    update

--- packages/solid-app/vite.config.ts
+++ packages/solid-app/vite.config.ts
@@ -5,11 +5,14 @@ import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      //@ts-ignore
+      adapter: vercel({ edge: true, prerender: true }),
+    }),
+  ],
 
   ssr: {
     external: ["@prisma/client"],
   },
-  //@ts-ignore
-  adapter: vercel({ edge: true, prerender: true }),
 });
--- packages/solid2/vite.config.ts
+++ packages/solid2/vite.config.ts
@@ -1,6 +1,13 @@
 import solid from "solid-start/vite";
+//@ts-ignore
+import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      //@ts-ignore
+      adapter: vercel({ edge: true, prerender: true }),
+    }),
+  ],
 });
